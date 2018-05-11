1a481fcd7b27a908be76fb80d1a3cf15122bd6b0
Kris Cox <kris.cox@cococo.be>
2018-05-11 23:04:11 +0200

    Initial check-in of module Syndicoco

---
+++ Syndicoco/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Syndicoco.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
